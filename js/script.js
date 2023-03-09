b6c69225a4fa3ab91356d1977255099e15d7a416
Kundyz <alikundyz1@gmail.com>
2023-03-09 21:07:31 +0600

    rest operator used

--- js/script.js
+++ js/script.js
@@ -174,12 +174,13 @@ window.addEventListener('DOMContentLoaded', () => {
    //используем классы для карточек
 
    class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector); //здесь лежит дом-эл-т,(родитель куда помещается конструкция созданная в render)
       this.transfer = 27;
       this.changeToUAH(); // метод  changeToUAH можем вызывать в методе кот-й будет что-то помещать на страницу либо прямо в конструкторе
@@ -193,9 +194,16 @@ window.addEventListener('DOMContentLoaded', () => {
       // метод  changeToUAH можем вызывать в методе кот-й будет что-то помещать на страницу либо прямо в конструкторе
       //создаем эл-т поместим в нее верстку, эту верстку дополним теми данными кот-е приходят,как аргументы и поместить этот эл-т уже на страницу
       const element = document.createElement('div');
+      if (this.classes.length === 0 ) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      
       // методом render() будем создавать структуру  кот-й помещается в определенный div
       element.innerHTML = `
-        <div class="menu__item">
             <img src=${this.src} alt=${this.alt}>
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">${this.descr}</div>
@@ -204,7 +212,6 @@ window.addEventListener('DOMContentLoaded', () => {
                 <div class="menu__item-cost">Цена:</div>
                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>
-        </div>  
       `;
       this.parent.append(element); // мы добавляем нов.созд.эл-т родителю
     }
@@ -221,7 +228,7 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    '.menu .container'
+    '.menu .container',
   ).render(); 
   //здесь создаем нов. объект и сразу же вызываем на нем метод render(он что-то отработает, и исчезнет)
   // объект может сущ. и без переменной, можем ее никуда не ложить.
@@ -233,7 +240,7 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню "Премиум"',
     'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     21,
-    '.menu .container'
+    '.menu .container',
   ).render(); 
 
 
@@ -243,7 +250,7 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню "Постное"',
    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     14,
-    '.menu .container'
+    '.menu .container',
   ).render(); 
 
   
